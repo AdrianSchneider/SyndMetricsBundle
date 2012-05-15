c729b606282d00f9d69de39fb91160188ab95b9b
Adrian Schneider <siradrian@gmail.com>
2012-05-15 13:10:04 -0700

    session integration, tracker entity

--- Entity/Metric.php
+++ Entity/Metric.php
@@ -2,7 +2,102 @@
 
 namespace Synd\MetricsBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ */
 class Metric
 {
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    protected $id;
     
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $event;
+    
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    protected $date;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $session;
+
+    /**
+     * Set event
+     *
+     * @param string $event
+     */
+    public function setEvent($event)
+    {
+        $this->event = $event;
+    }
+
+    /**
+     * Get event
+     *
+     * @return string 
+     */
+    public function getEvent()
+    {
+        return $this->event;
+    }
+
+    /**
+     * Set date
+     *
+     * @param datetime $date
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+    }
+
+    /**
+     * Get date
+     *
+     * @return datetime 
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set session
+     *
+     * @param string $session
+     */
+    public function setSession($session)
+    {
+        $this->session = $session;
+    }
+
+    /**
+     * Get session
+     *
+     * @return string 
+     */
+    public function getSession()
+    {
+        return $this->session;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
 }
--- Tracker/Tracker.php
+++ Tracker/Tracker.php
@@ -3,10 +3,17 @@
 namespace Synd\MetricsBundle\Tracker;
 
 use Synd\MetricsBundle\Entity\Metric;
+use Symfony\Component\EventDispatcher\Event;
+use Symfony\Component\HttpFoundation\Session;
 use Doctrine\ORM\EntityManager;
 
 class Tracker
 {
+    /**
+     * @var    Session
+     */
+    protected $session;
+    
     /**
      * @var    EntityManager
      */
@@ -14,25 +21,27 @@ class Tracker
     
     /**
      * Brings deps into scope
+     * @param    Session
      * @param    EventityManager
      */
-    public function __construct(EntityManager $em)
+    public function __construct(Session $session, EntityManager $em)
     {
+        $this->session = $session;
         $this->em = $em;
     }
     
     /**
      * Stores a trackable metric
+     * @param    Event
      * @param    string        Event Name
      */
-    public function track($eventName)
+    public function track(Event $event, $eventName)
     {
         $metric = new Metric();
-        #$metric->setEvent($eventName);
-        #$metric->setDate(new \DateTime());
-        #$metric->setUser( null ); // TODO
-        #$metric->setSession( null ); // TODO
-        return;
+        $metric->setEvent($eventName);
+        $metric->setSession($this->session->getId());
+        $metric->setDate(new \DateTime());
+
         $this->em->persist($metric);
         $this->em->flush();
     }
