adb444b551fbd04b38e3bb89c6aa58e1f15b378d
Adrian Schneider <siradrian@gmail.com>
2012-05-16 13:46:45 -0700

    reminder to clear cahce

--- Command/SyncMetricsCommand.php
+++ Command/SyncMetricsCommand.php
@@ -3,6 +3,7 @@
 namespace Synd\MetricsBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -21,5 +22,6 @@ class SyncMetricsCommand extends ContainerAwareCommand
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $this->getContainer()->get('synd_metrics.sync')->sync();
+        $output->writeln("Metrics synchronized successfully.  It's advised to run <info>cache:clear</info> to ensure DIC is functioning.");
     }
 }
